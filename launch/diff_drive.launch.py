6769f908b1d096c7eb733f9a0443207c3a5773e3
ahmetraufoktay <68822176+ahmetraufoktay@users.noreply.github.com>
2025-09-17 13:55:36 +0300

    add : diff_drive.launch.py and meshes

---
+++ launch/diff_drive.launch.py
@@ -0,0 +1,42 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    use_sim_time_arg = DeclareLaunchArgument(
+        name='use_sim_time',
+        default_value='true',
+        description='Use simulation (Gazebo) clock if true'
+    )
+
+    package_arg = DeclareLaunchArgument('urdf_package',
+                                        description='The package where the robot description is located',
+                                        default_value='rover_sim')
+    model_arg = DeclareLaunchArgument('urdf_package_path',
+                                      description='The path to the robot description relative to the package root',
+                                      default_value='urdf/model.urdf.xacro')
+    
+    return LaunchDescription([
+        use_sim_time_arg,
+        package_arg,
+        model_arg,
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(PathJoinSubstitution([FindPackageShare("rover_sim"), "launch", "gazebo.launch.py"])),
+            launch_arguments={
+                "use_sim_time" : LaunchConfiguration('use_sim_time'),
+                "urdf_package" : LaunchConfiguration('urdf_package'),
+                "urdf_package_path" : LaunchConfiguration('urdf_package_path')
+            }.items()
+        ),
+        Node(
+            package='rqt_robot_steering',
+            executable='rqt_robot_steering',
+            remappings=[
+                ('/cmd_vel', '/rover/cmd_vel')
+            ]
+        )
+    ])
